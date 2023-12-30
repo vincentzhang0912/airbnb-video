3a5f311a04c437de47900464c0b12a935503c34a
vincent <vincentzys0912@gmail.com>
2023-12-30 00:32:57 -0800

    Login function

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,9 @@ import './globals.css';
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasteProvider";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -15,18 +17,21 @@ export const metadata: Metadata = {
   description: 'Airbnb clone',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser= await getCurrentUser();
+  
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser = {currentUser}/>
         </ClientOnly>
         {children}
       </body>
